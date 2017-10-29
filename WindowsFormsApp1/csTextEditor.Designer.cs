e29d9893d46bbc5371d196f0bd99c20e388d19fa
Ian Golden <ian@iangolden.com>
2017-10-29 13:50:47 -0600

    Committing source work from CS text editor

---
+++ WindowsFormsApp1/csTextEditor.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class csTextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.treeView1.ForeColor = System.Drawing.Color.Yellow;
+            this.treeView1.HotTracking = true;
+            this.treeView1.Location = new System.Drawing.Point(12, 38);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(228, 563);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Files";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(677, 563);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button2.Location = new System.Drawing.Point(861, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Tag = "saveButton";
+            this.button2.Text = "Save File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Active File: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select a file from the tree";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // csTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(948, 613);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.ForeColor = System.Drawing.Color.Lavender;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
+            this.Name = "csTextEditor";
+            this.Text = "CSharp Text Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/csTextEditor.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class csTextEditor : Form
+    {
+        // declare private 'globals' here
+        private FolderBrowserDialog folderBrowserDialog1;
+        private RichTextBox richTextBox1;
+        private OpenFileDialog openFileDialog1;
+        private string openFileName, folderName;
+        private bool fileOpened = false;
+
+ 
+
+        public csTextEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("blah");
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            FileAttributes attr = File.GetAttributes(e.Node.Text);
+            if (attr == FileAttributes.Directory)
+            {
+                String[] dirFiles = Directory.GetFileSystemEntries(e.Node.Text);
+                for (int i = 0; i < dirFiles.Length; i++)
+                {
+                    TreeNode dirFileNode = new TreeNode();
+                    dirFileNode.Text = dirFiles[i];
+                    e.Node.Nodes.Add(dirFileNode);
+                   
+                }
+                e.Node.Expand();
+            }
+            else
+            {
+                if (Path.GetExtension(e.Node.Text) == ".txt")
+                {
+                    label3.Text = e.Node.Text;
+                    textBox1.Text = File.ReadAllText(e.Node.Text);
+                } else
+                {
+                    MessageBox.Show("We don't support that file.\n Chill out bro.");
+                }
+            }
+
+            // pseudocode
+
+            // if Directory.GetFiles();d
+            //  checkForNodes();
+            // else
+            //  openTreeNode();
+            // end
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_InitLayout()
+        {
+            button2.ForeColor = ColorTranslator.FromHtml("#000000");
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String activePath = label3.Text;
+            String[] fileLines = textBox1.Lines;
+            // save the file
+            File.WriteAllLines(activePath, fileLines);
+            MessageBox.Show("Saved Successfully");
+        }
+
+
+        private void button1_Click(object sender, System.EventArgs e)
+            {
+                Stream myStream = null;
+                FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                TreeNode treeNode = new TreeNode();
+                treeNode.Tag = folderBrowserDialog1.SelectedPath;
+                treeNode.Text = folderBrowserDialog1.SelectedPath;
+                treeView1.Nodes.Add(treeNode);
+
+            if (result == DialogResult.OK)
+            {
+                String[] filesArr = Directory.GetFileSystemEntries(folderBrowserDialog1.SelectedPath);
+                for (int i = 0; i < filesArr.Length; i++)
+                {
+                    string s = filesArr[i];
+                    if (File.GetAttributes(filesArr[i]) == FileAttributes.Directory)
+                    {
+                        TreeNode dirNode = new TreeNode();
+                        dirNode.Text = filesArr[i];
+                        treeNode.Nodes.Add(filesArr[i]);                       
+                    }
+                    else
+                    {
+                        TreeNode fileNode = new TreeNode();
+                        fileNode.Text = filesArr[i];
+                        if (Path.GetExtension(fileNode.Text) == ".txt")
+                        {
+                            fileNode.ForeColor = ColorTranslator.FromHtml("#ffffff");
+
+                        }
+                        else
+                        {
+                            fileNode.ForeColor = ColorTranslator.FromHtml("#ff0000");
+
+                        }
+                        treeNode.Nodes.Add(fileNode);
+
+
+                    }
+                }
+                treeView1.ExpandAll();
+            }
+        }
+    }
+}
